8b2c5531277baf25609a957f6886ba5ff773cef2
Gleb <zadachings@gmail.com>
2018-12-09 17:41:06 +0200

    Added Edit on Flights

--- AviaSite/Controllers/HomeController.cs
+++ AviaSite/Controllers/HomeController.cs
@@ -12,22 +12,36 @@ namespace AviaSite.Controllers
 
         public ActionResult Index()
         {
+            var planes = db.Planes.ToList();
+            var compamies = db.Companies.ToList();
             var schedule = db.Flights.ToList();
             return View(schedule);
         }
 
-        public ActionResult About()
+        [HttpGet]
+        public ActionResult Edit(int id)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            SelectList planes = new SelectList(db.Planes.Where(x=>x.InFlight == false).Select(x=>x.PlaneNum));
+            
+            List<SelectListItem> planesList = new List<SelectListItem>(planes);
+            Plane plane = db.Flights.Where(x => x.flight1 == id).Select(x=>x.Plane1).First();
+            planesList.Add(new SelectListItem() { Text = plane.PlaneNum.ToString(), Value = plane.PlaneNum.ToString(), Selected = true });
+            ViewBag.Planes = planesList;
+            var flight = db.Flights.Where(x => x.flight1 == id).First();
+            return View(flight);
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult Edit(Flight flight)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            foreach (var item in db.Planes)
+            {
+                if (!db.Flights.Select(x => x.Plane).Contains(item.PlaneNum))
+                    item.InFlight = false;
+            }
+            db.Entry(flight).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
